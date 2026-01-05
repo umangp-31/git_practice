45177c0b540a600a004dcf7fdd17bd7388454735
umang <umangp@webelight.co.in>
2026-01-05 12:22:30 +0530

    change n branch demo

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 try:
     num = int(input("Enter number"))
-
     if num%2==0:
         print("Even number")
     else:
         print("Odd number")
-except Exception as e:
+except ValueError as e:
     print(e)
